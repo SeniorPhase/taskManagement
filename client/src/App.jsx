f898135e2b4abf7dda39c1dfb23620db2b8ffdf5
medban <bannourmohamedabdesslam@gmail.com>
2024-07-15 21:13:59 +0100

    new

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from './login/login'
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <Login/>
+    </div>
+     
+    </>
+  );
+}
+
+export default App;
